2ba5250eeeaf863e29d92cb7d4baedae71d24c08
Scott Fessler <sfessler@umich.edu>
2018-01-31 19:33:42 -0500

    Projects and Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 	resources:posts
+	resources:projects
+	resources:contacts
 	get 'index' => 'welcome#index'
 	root 'welcome#index'
+	get '*path' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
